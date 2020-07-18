e40a6b699bd1211a55b801c2a67fdfa60ebcf304
Yarik1993 <makarov1603@icloud.com>
2020-07-18 16:40:56 +0300

    Updates

---
+++ precache-manifest.2db8940a8b8f7c9b68e205254cb43061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1353b3d834aa97c39da0aa5845c64cea",
+    "url": "/star_wars/index.html"
+  },
+  {
+    "revision": "56672633283d48496008",
+    "url": "/star_wars/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "5a60162e006b89a90444",
+    "url": "/star_wars/static/js/2.1aec8a77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star_wars/static/js/2.1aec8a77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56672633283d48496008",
+    "url": "/star_wars/static/js/main.22db34d0.chunk.js"
+  },
+  {
+    "revision": "6f6bc12bc03513f0b557",
+    "url": "/star_wars/static/js/runtime-main.bda6e186.js"
+  }
+]);
---
+++ static/js/main.22db34d0.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpstar_wars=this.webpackJsonpstar_wars||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),s=n(2),c=n.n(s);n(8);var l=function(){return r.a.createElement("div",{className:"App"},r.a.createElement("h1",null,"Hello"))};c.a.render(r.a.createElement(l,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.22db34d0.chunk.js.map
